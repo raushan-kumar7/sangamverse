cb0081f1b7930a17e927fa0d8b834c808b41ffe4
Raushan Kumar <raushan911721@gmail.com>
2024-10-29 23:26:06 +0530

    initial comment

---
+++ assets/php/config.php
@@ -0,0 +1,13 @@
+<?php
+session_start();
+
+// mysqli database connection
+// const DB_NAME = 'if0_37600049_sangam_verse	';
+// const DB_HOST = 'sql311.infinityfree.com';
+// const DB_USER = 'if0_37600049';
+// const DB_PASS = '4S6u8GKvFKTa';
+
+const DB_NAME = 'sangam_verse';
+const DB_HOST = 'localhost';
+const DB_USER = 'root';
+const DB_PASS = '';
